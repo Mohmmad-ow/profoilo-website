accf3063fcb1742627233d5bd4515b24bc9d03d2
Mohmmad-ow <moemessi00020@gmail.com>
2022-08-11 10:24:43 +0300

    delete and tags table/test for color input

--- app.js
+++ app.js
@@ -15,21 +15,45 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/portfolioDB");
 
+
+
+
+// Tags schema
+
+const tagsSchema = mongoose.Schema({
+    name: {type: String, required: true, unique: true },
+    colorValue: {type: String, default: '#000'}
+});
+
+const Tags = mongoose.model('tag', tagsSchema);
+
+// project-blog schema
 const projectsSchema = mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String},
     date: { type: Date, required: true, default: Date() },
-    updateDate: {type: Date}
+    updateDate: {type: Date},
+    tags: [tagsSchema]
 });
 const Projects = mongoose.model('project', projectsSchema);
 
+
+// tags-in-project
+const tag1 = new Tags({
+    name: "TypeScript",
+    colorValue: "#000"
+});
+
 const pro1 = new Projects({
-    title: "webScrape",
-    description: "scrapes data"
+    title: "crape",
+    description: "scrapes data",
+    tags: [tag1]
 });
 
 app.get("/", (req, res) => {
     res.render("index")
+    tag1.save();
+    pro1.save();
 
 })
 
@@ -60,6 +84,8 @@ app.post ('/projects/create',(req, res) => {
 
 // View all projects
 
+
+// options for how to view time
 const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' , hour: 'numeric', minute: 'numeric'};
 
 app.get('/projects', (req, res) => {
@@ -135,6 +161,55 @@ app.post('/projects/:id/update', (req, res) => {
     });
 })
 
+// Delete a project
+
+
+// _____view the post you want to delete______
+
+app.get('/projects/:id/delete', (req, res) => {
+    const projectId = req.params.id;
+
+    Projects.findById(projectId, (err, doc) => {
+        if (!err) {
+            if (doc) {
+            res.render('project-blog-delete', {project: doc});
+            } else {
+                res.send("<h1>Didn't find</h1>");
+            }
+        } else {
+            console.log(err);
+        }
+    });
+
+});
+
+app.post('/projects/:id/delete', (req, res) => {
+    const projectId = req.params.id;
+
+    Projects.findByIdAndDelete(projectId, (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send("Project blog deleted");
+        }
+    });
+    
+});
+
+// Test for tags color
+
+app.get("/colors", (req, res) => {
+    res.render('color-input-test')
+});
+
+app.post("/colors", (req, res) => {
+    console.log(req.body)
+})
+
+
+
+
+// Running server
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
