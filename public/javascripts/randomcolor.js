b1ed0ceff0903d27c8159647a558d48adec04229
Mohmmad-ow <moemessi00020@gmail.com>
2022-08-22 10:16:16 +0300

    project gird design

--- public/javascripts/randomcolor.js
+++ public/javascripts/randomcolor.js
@@ -1,7 +1,7 @@
 let randomColors = ["#C21010","#277BC0","#54BAB9","#002B5B","#781C68","#F65A83"];
 
 function chooseRandomColor() {
-    return Math.floor(Math.random() * 6) + 1;
+    return Math.floor(Math.random() * 6);
 }
 
 let tags = document.getElementsByClassName("grid-container");
@@ -11,5 +11,6 @@ for(let i =0; tags.length > i; i++) {
     let color = chooseRandomColor();
     console.log(color)
     tags[i].style.backgroundColor = randomColors[color];
+    tags[i].style.boxShadow = '2px 2px 10px '+ randomColors[color];
 }
 
